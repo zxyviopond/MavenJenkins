db9d46019f645b5185b5454fd4047c4f9e5b827e
xiaoyunz <xiaoyunz@twitter.com>
2013-10-28 10:17:01 -0700

    my first commit to check maven to jenkins

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+@RunWith(JUnit4.class)
+public class MainTest{
+
+	@Test
+	public void test() {
+		assertEquals("Heelo","Heelo");
+	}
+	
+	@Test
+	public void secondtest() {
+		assertEquals("Heelo","Heelo");
+	}
+
+}
